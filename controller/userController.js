d491023975fb6daf2068d9d7741190c97ea14a18
mohammedafrid375 <mohammedafrid375@gmail.com>
2025-02-17 22:47:16 +0530

    Basic requirement files added

---
+++ controller/userController.js
@@ -0,0 +1,19 @@
+import User from '../models/user.js';
+const registerUser = async (req, res) => {
+  const { name, email, password } = req.body;
+  try {
+    const user = await User.create({ name, email, password });
+    res.status(201).json(user);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
+const getUsers = async (req, res) => {
+  try {
+    const users = await User.find();
+    res.json(users);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+export { registerUser, getUsers };
---
+++ routes/UserRoute.js
@@ -0,0 +1,6 @@
+import express from 'express';
+import { registerUser, getUsers } from '../controller/userController.js';
+const router = express.Router();
+router.post('/register', registerUser);
+router.get('/', getUsers);
+export default router;
